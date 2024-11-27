9a9442c5a590b84476a5f2e7b83ba94efcb3ddce
MelsinOC <melsinorozco82@gmail.com>
2024-11-27 16:09:25 -0600

    diseño de el menu he inventario

--- CelltechFinal/Views/Menu/MenuPrincipal.xaml.cs
+++ CelltechFinal/Views/Menu/MenuPrincipal.xaml.cs
@@ -1,14 +1,87 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using CelltechFinal.ViewModels;
 
 namespace CelltechFinal.Views.Menu
 {
     public partial class MenuPrincipal : Window
     {
+        private bool isMaximized = false;
+        private readonly TextBlock maximizeIcon;
+        private const WindowState maximized = WindowState.Maximized;
+
         public MenuPrincipal()
         {
             InitializeComponent();
             DataContext = new MenuPrincipalViewModel();
+
+            // Iniciar maximizado
+            WindowState = maximized;
+
+
+            // Inicializar el ícono de maximizar
+            maximizeIcon = FindName("MaximizeIcon") as TextBlock;
+            if (maximizeIcon != null)
+            {
+                maximizeIcon.Text = "❐";
+            }
         }
+
+        private void CenterWindow()
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            double left = (screenWidth - this.Width) / 2;
+            double top = (screenHeight - this.Height) / 2;
+
+            this.Left = left;
+            this.Top = top;
+        }
+
+
+        #region Botones de control de ventana
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isMaximized)
+            {
+                // Restaurar
+                WindowState = WindowState.Normal;
+                Width = 1080; // Restaurar a 1080
+                Height = 650; // Restaurar a 720
+                CenterWindow();
+                isMaximized = false;
+
+                if (maximizeIcon != null)
+                {
+                    maximizeIcon.Text = "☐";
+                }
+            }
+            else
+            {
+                // Maximizar
+                WindowState = WindowState.Maximized;
+                isMaximized = true;
+
+                if (maximizeIcon != null)
+                {
+                    maximizeIcon.Text = "❐";
+                }
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        #endregion
+
+
     }
 }
--- CelltechFinal/Views/Ventas/NuevaVentaWindow.xaml.cs
+++ CelltechFinal/Views/Ventas/NuevaVentaWindow.xaml.cs
@@ -1,26 +1,89 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using CelltechFinal.ViewModels;
 
 namespace CelltechFinal.Views.Ventas
 {
     public partial class NuevaVentaWindow : Window
+
     {
+
+
+        private bool isMaximized = false;
+        private readonly TextBlock maximizeIcon;
+        private const WindowState maximized = WindowState.Maximized;
+
         public NuevaVentaWindow()
         {
             InitializeComponent();
             DataContext = new NuevaVentaViewModel();
+            // Iniciar maximizado
+            WindowState = maximized;
+
+
+            // Inicializar el ícono de maximizar
+            maximizeIcon = FindName("MaximizeIcon") as TextBlock;
+            if (maximizeIcon != null)
+            {
+                maximizeIcon.Text = "❐";
+            }
+        }
+
+        private void CenterWindow()
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            double left = (screenWidth - this.Width) / 2;
+            double top = (screenHeight - this.Height) / 2;
+
+            this.Left = left;
+            this.Top = top;
+        }
+
+
+        #region Botones de control de ventana
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isMaximized)
+            {
+                // Restaurar
+                WindowState = WindowState.Normal;
+                Width = 1080; // Restaurar a 1080
+                Height = 650; // Restaurar a 720
+                CenterWindow();
+                isMaximized = false;
+
+                if (maximizeIcon != null)
+                {
+                    maximizeIcon.Text = "☐";
+                }
+            }
+            else
+            {
+                // Maximizar
+                WindowState = WindowState.Maximized;
+                isMaximized = true;
+
+                if (maximizeIcon != null)
+                {
+                    maximizeIcon.Text = "❐";
+                }
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            Close();
         }
 
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
+
+        #endregion
 
         private void TxtCodigoProducto_PreviewKeyDown(object sender, KeyEventArgs e)
         {
@@ -32,5 +95,7 @@ namespace CelltechFinal.Views.Ventas
                 }
             }
         }
+
+
     }
 }
